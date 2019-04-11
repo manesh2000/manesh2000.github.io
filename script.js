72160b5375e4da8dc0fc4b3382f4e50476b808b8
manesh2000 <manesh.family@gmail.com>
2019-04-11 16:01:09 +0530

    js-test
    just a simple java script test to fool my friend

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var firstName = prompt("What is your First Name?") ;
+
+
+var lastName = prompt("What is your Last Name?") ;
+
+
+var age = prompt("What is your Age?") ;
+
+
+console.log("Your Full Name is " + firstName + " " + lastName) ;
+
+console.log("You are " + age + " " + "years old") ;
+
+alert("Hello MR.Kundan " + firstName) ;
+
